2ab1611f4e3281fae01ec126c2e149793711ad0b
陳建佑 <97875966+ChienYuYu@users.noreply.github.com>
2022-09-25 17:06:50 +0800

    osm sample

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue';
+// import L from 'leaflet';
 import App from './App.vue';
 import router from './router';
+import 'leaflet/dist/leaflet';
+import 'leaflet/dist/leaflet.css';
 
 createApp(App).use(router).mount('#app');
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper">
+    <div class="search-area d-flex justify-content-center align-items-center border">
+      <input type="text" aria-label="searchbar" placeholder="請輸入區或路名" />
+    </div>
+    <div id="map"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
+import L from 'leaflet';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.min';
+import 'leaflet.locatecontrol/dist/L.Control.Locate.css';
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld,
+  mounted() {
+    const map = L.map('map').setView([22.6563171, 121.4711553], 15);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '© OpenStreetMap',
+    }).addTo(map);
+
+    L.control.locate().addTo(map);
   },
 };
 </script>
+
+<style lang="scss">
+.wrapper {
+  .search-area {
+    height: 100px;
+    background: #333333;
+  }
+  #map{
+    height: calc(100vh - 100px);
+  }
+}
+</style>
